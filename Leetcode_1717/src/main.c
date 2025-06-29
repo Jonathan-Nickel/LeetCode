18391b3c403adc65e70d6ccbccf297e793ca4009
Jonathan Nickel <jgnickel@inf.ufpel.edu.br>
2025-06-29 20:10:12 -0300

    main.c

--- Leetcode_1717/src/main.c
+++ Leetcode_1717/src/main.c
@@ -16,6 +16,22 @@ int main() {
     return 0;
 }
 
+/*
+================================================================================
+maximumGain
+ Guarda o tamanho da string s em sizeS.
+ Define as variaveis first, second, high e low.
+ define first and second como 'b' e 'a' se x for menor que y,
+ ou como 'a' e 'b' se x for maior ou igual a y.
+ Cria uma pilha e inicializa top e score em 0.
+ se o topo for maior que 0 e o ultimo elemento do stack for igual a first e o elemento atual for igual 
+ a second, remove o ultimo elemento do stack, incrementa score por high e continua.
+ Caso contrario, adiciona o elemento atual ao stack e incrementa top.
+ Repete o processo para a segunda pilha stack2, onde first e second são trocados.
+ Libera a memoria alocada para stack e stack2.
+ Retorna o valor de score.
+================================================================================
+*/
 int maximumGain(char* s, int x, int y) {
 
     int sizeS = strlen ( s );
